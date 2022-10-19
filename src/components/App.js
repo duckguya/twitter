1d004599d48adeb5e37ee061104c2914b25f8688
duckguya <duckguya@gmail.com>
2022-10-19 17:34:35 +0900

    Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // 유저정보가 없으면 null이 반환된다.
+
+  // firebase가 프로그램을 초기화하면 setInit해준다. (회원가입 로그인)
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} twitter</footer>
     </>
   );
