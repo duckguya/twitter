84c0cb63499d026495b71be64eb899a6b90b4811
duckguya <duckguya@gmail.com>
2022-10-20 19:50:19 +0900

    Database Setup and Tweeting

--- src/fbase.js
+++ src/fbase.js
@@ -18,3 +18,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,35 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-export default () => <span>Home</span>;
+export default () => {
+  const [tweet, setTweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
